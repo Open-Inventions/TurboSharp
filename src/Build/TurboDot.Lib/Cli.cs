9cec0b96949723568f648493c122ca3d54838cf5
Hubertus Sack <hubertussack@outlook.com>
2023-01-03 17:32:52 +0100

    Adding cli class to help with starting dot

---
+++ src/Build/TurboDot.Lib/Cli.cs
@@ -0,0 +1,15 @@
+﻿using System.CommandLine;
+using System.Threading.Tasks;
+using TurboDot.Tools;
+
+namespace TurboDot
+{
+    public static class Cli
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            var rootCommand = Parser.Build(Parser.RootCommand);
+            return await rootCommand.InvokeAsync(args);
+        }
+    }
+}
